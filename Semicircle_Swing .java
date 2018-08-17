7c486995d56ce30aa4dbf964966e65a31240600f
Haarini Rao <39938842+Haarini@users.noreply.github.com>
2018-08-17 18:18:09 +0530

    Create Semicircle_Swing .java

---
+++ Semicircle_Swing .java
@@ -0,0 +1,26 @@
+import javax.swing.*; 
+import java.awt.*; 
+
+//To draw a semicircle using swing
+public class Semicircle_Swing extends JPanel
+{ 
+public void paint(Graphics g){ 
+ 
+	g.setColor(Color.GREEN); 
+	g.fillArc(50, 50, 100, 100, 0, 180); 
+	g.setColor(Color.BLUE); 
+	g.fillArc(200, 100, 100,100,0,180); 
+	g.setColor(Color.RED);
+	g.fillOval(400, 50, 100, 100);
+
+} 
+public static void main(String[] args){ 
+JFrame jf=new JFrame(); 
+jf.setDefaultCloseOperation 
+(JFrame.EXIT_ON_CLOSE); 
+jf.setPreferredSize(new Dimension(300,500)); 
+jf.add(new Test()); 
+jf.pack(); 
+jf.setVisible(true); 
+} 
+} 
